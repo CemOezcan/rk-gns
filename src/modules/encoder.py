344a85d79387cb2cd66d7f9efa496e8085298c64
Cem Özcan <cem.oezcan@outlook.com>
2023-06-03 16:48:25 +0200

    Implement PyG-Encoder.

--- src/modules/encoder.py
+++ src/modules/encoder.py
@@ -1,7 +1,8 @@
-from typing import Callable, List
+from typing import Callable, List, Tuple
 
 import torch
 from torch import nn
+from torch_geometric.data import HeteroData
 
 from src.util.types import MultiGraph
 
@@ -9,39 +10,25 @@ from src.util.types import MultiGraph
 class Encoder(nn.Module):
     """Encodes node and edge features into latent features."""
 
-    def __init__(self, make_mlp: Callable, latent_size: int, edge_sets: List[str], hierarchical=True):
+    def __init__(self, make_mlp: Callable, latent_size: int, node_sets: List[str], edge_sets: List[str], hierarchical=True):
         super().__init__()
         self._make_mlp = make_mlp
         self._latent_size = latent_size
 
-        self.node_model = self._make_mlp(latent_size)
+        self.node_model = nn.ModuleDict({name: self._make_mlp(latent_size) for name in node_sets})
         self.edge_models = nn.ModuleDict({name: self._make_mlp(latent_size) for name in edge_sets})
         self.hierarchical = hierarchical
 
         if hierarchical:
             self.hyper_node_model = self._make_mlp(latent_size)
 
-    def forward(self, graph: MultiGraph) -> MultiGraph:
-        node_latents = [self.node_model(graph.node_features[0])]
-        if self.hierarchical:
-            try:
-                node_latents.append(self.hyper_node_model(graph.node_features[1]))
-            except (IndexError, AttributeError):
-                pass
-        else:
-            try:
-                node_latents.append(self.node_model(graph.node_features[1]))
-            except (IndexError, AttributeError):
-                pass
+    def forward(self, graph: HeteroData) -> HeteroData:
+        for position, node_type in enumerate(graph.node_types):
+            graph.node_stores[position]["x"] = self.node_input_embeddings[node_type](
+                graph.node_stores[position]["x"])
 
+        for position, edge_type in enumerate(graph.edge_types):
+            graph.edge_stores[position]["edge_attr"] = self.edge_input_embeddings[edge_type](
+                graph.edge_stores[position]["edge_attr"])
 
-        new_edges_sets = []
-        for edge_set in graph.edge_sets:
-            feature = edge_set.features
-            try:
-                latent = self.edge_models[edge_set.name](feature)
-                new_edges_sets.append(edge_set._replace(features=latent))
-            except KeyError:
-                continue
-
-        return MultiGraph(node_latents, new_edges_sets)
+        return graph
