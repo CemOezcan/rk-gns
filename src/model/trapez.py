Fix edge normalization.

src/model/trapez.py
@@ -31,7 +31,7 @@ class TrapezModel(AbstractSystemModel):
         self.loss_fn = torch.nn.MSELoss()
 
         self._output_normalizer = Normalizer(size=2, name='output_normalizer')
-        self._mesh_edge_normalizer = Normalizer(size=13, name='mesh_edge_normalizer', device='cpu').cpu()
+        self._mesh_edge_normalizer = Normalizer(size=13, name='mesh_edge_normalizer')
 
         self.message_passing_steps = params.get('message_passing_steps')
         self.message_passing_aggregator = params.get('aggregation')
@@ -75,7 +75,6 @@ class TrapezModel(AbstractSystemModel):
             data = self.add_noise_to_mesh_nodes(data, self.input_mesh_noise)
         data = self.add_noise_to_pcd_points(data, self.input_pcd_noise)
         data = self.transform_position_to_edges(data, self.euclidian_distance)
-        data.edge_attr = self._mesh_edge_normalizer(data.edge_attr, is_training)
 
         edge_index = data.edge_index
         edge_attr = data.edge_attr
@@ -104,12 +103,13 @@ class TrapezModel(AbstractSystemModel):
 
         return hetero_data
 
-    def forward(self, graph):
+    def forward(self, graph, is_training):
+        graph[('mesh', '0', 'mesh')].edge_attr = self._mesh_edge_normalizer(graph[('mesh', '0', 'mesh')].edge_attr, is_training)
         return self.learned_model(graph)
 
     def training_step(self, graph: Batch):
         graph.to(device)
-        pred_velocity, _ = self(graph)
+        pred_velocity, _ = self(graph, True)
         target_velocity = self.get_target(graph, True)
 
         loss = self.loss_fn(target_velocity, pred_velocity)
@@ -125,7 +125,7 @@ class TrapezModel(AbstractSystemModel):
     @torch.no_grad()
     def validation_step(self, graph: Batch, data_frame: Dict) -> Tuple[Tensor, Tensor]:
         graph.to(device)
-        pred_velocity = self(graph)[0]
+        pred_velocity = self(graph, False)[0]
         target_velocity = self.get_target(graph, False)
         error = self.loss_fn(target_velocity, pred_velocity).cpu()
 
@@ -196,7 +196,7 @@ class TrapezModel(AbstractSystemModel):
         graph = Batch.from_data_list([self.build_graph(data, is_training=False, keep_point_cloud=keep_pc)]).to(device)
         data = data[0] if keep_pc else data[1]
 
-        output, hidden = self(graph)
+        output, hidden = self(graph, False)
 
         prediction, cur_position, cur_velocity = self.update(data.to(device), output[mask])
         next_pos[mask] = prediction