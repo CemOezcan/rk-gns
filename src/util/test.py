6da3fb2072dafa75275c7275365504570a4140ef
Cem Özcan <cem.oezcan@outlook.com>
2023-10-08 22:52:32 +0200

    Optimize preprocessing.

--- src/util/test.py
+++ src/util/test.py
@@ -45,19 +45,19 @@ def visualize_graph(data):
     #ax.add_collection(gt_collection)
 
     #ax.scatter(gt_pos[mask][:, 0], gt_pos[mask][:, 1], label='Ground Truth Position', c='blue', alpha=0.5, s=5)
-    ax.scatter(gt_pos[:, 0], gt_pos[:, 1], label='Ground Truth Position', c='blue', alpha=0.5, s=5)
+    ax.scatter(gt_pos[:, 0], gt_pos[:, 1], label='Ground Truth Position', c='dimgray', alpha=0.3, s=5)
 
     #ax.scatter(data.this[:, 0], data.this[:, 1], label='Ground Truth Position', c='red', alpha=0.5, s=5)
     #ax.scatter(gt_pos[collider_mask][:, 0], gt_pos[collider_mask][:, 1], label='Ground Truth Position', c='dimgrey', alpha=0.5, s=5)
     #ax.scatter(gt_pos[point_index:][:, 0], gt_pos[point_index:][:, 1], label='Ground Truth Position', c='green', s=5)
 
-    colors = ['dimgrey', 'red', 'blue', 'green', 'yellow', 'red', 'brown', 'blue', 'dimgrey', 'red', 'blue', 'green', 'yellow', 'red', 'brown', 'blue']
+    colors = ['dimgrey', 'yellow', 'blue', 'blue', 'green', 'green', 'purple', 'purple', 'orange', 'red', 'cyan', 'cyan', 'lime', 'brown']
     for i, (x, y) in enumerate(zip(subgraph[('mesh', '0', 'mesh')].edge_index[0], subgraph[('mesh', '0', 'mesh')].edge_index[1])):
         if int(subgraph[('mesh', '0', 'mesh')].edge_type[i]) <= 100:
             fst = gt_pos[x]
             snd = gt_pos[y]
             color = colors[int(subgraph[('mesh', '0', 'mesh')].edge_type[i])]
-            ax.add_line(Line2D([fst[0], snd[0]], [fst[1], snd[1]], alpha=0.3, color=color))
+            ax.add_line(Line2D([fst[0], snd[0]], [fst[1], snd[1]], alpha=0.1, color=color))
 
 
     ax.legend()
