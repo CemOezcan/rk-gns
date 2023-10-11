b0ed8d8fe91c9e9f9a2a52150029644146e2efac
Cem Özcan <cem.oezcan@outlook.com>
2023-10-12 01:41:19 +0200

    Config changes.

--- src/modules/rkn/RKNCell.py
+++ src/modules/rkn/RKNCell.py
@@ -53,7 +53,7 @@ class RKNCell(nn.Module):
     def get_default_config() -> ConfigDict:
         config = {
             'num_basis': 16,
-            'bandwidth': 50,
+            'bandwidth': 32,
             'trans_net_hidden_units':[128, 128],
             'trans_net_hidden_activation':"ReLU",
             'learn_trans_covar':True,
