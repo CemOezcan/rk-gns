403050c32aa69d34044744f2886e1ec4c87b93a6
Cem Özcan <cem.oezcan@outlook.com>
2023-10-28 02:25:51 +0200

    Add point cloud noise to name.

--- src/algorithms/mesh_task.py
+++ src/algorithms/mesh_task.py
@@ -82,8 +82,9 @@ class MeshTask(AbstractTask):
         reduced = config.get('task').get('reduced')
         subsampling = config.get('task').get('subsampling')
         random_seed = config.get('random_seed')
+        pc_noise = config.get('model').get('pc_noise')
         #self._task_name = f'm:{self.task_type}_l:{layers}_fn:{feature_norm}_ln:{layer_norm}_b:{batch_size}_t:{self.model_type}_a:{aggr}_lr:{lr}_seq:{seq}_ggns:{ggns}_red:{reduced}_poisson:{poisson}_mp:{self._mp}_epoch:'
-        self._task_name = f'm:{self.task_type}_b:{batch_size}_t:{self.model_type}_lr:{lr}_wd:{wd}_seq:{seq}_ggns:{ggns}_red:{reduced}_p:{poisson}_r:{rnn_type}_mp:{self._mp}_s:{subsampling}_rand:{random_seed}_epoch:'
+        self._task_name = f'm:{self.task_type}_b:{batch_size}_t:{self.model_type}_lr:{lr}_wd:{wd}_seq:{seq}_ggns:{ggns}_red:{reduced}_p:{poisson}_r:{rnn_type}_mp:{self._mp}_s:{subsampling}_rand:{random_seed}_n{pc_noise}_epoch:'
 
         self.frequency_list = [0] if self.task_type != 'poisson' and not ggns and self.model_type == 'mgn' else get_from_nested_dict(config, ['task', 'imputation'])
 
